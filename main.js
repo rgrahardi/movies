678a469304c1e475bd7fc34cb113ab52d1540550
rgrahardi <rgrahardi2019@gmail.com>
2021-10-27 09:51:59 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+const container=document.querySelector(".container")
+
+async function getApi(){
+    try {
+        let response=await fetch('https://ghibliapi.herokuapp.com/films')
+        let result=await response.json()
+        //console.log(result);
+        return result
+    } catch (error) {
+        console.log('error');
+    }
+}
+
+function display(movies){
+    let result=""
+    movies.forEach(movie => {
+        result+=`
+        <div class="card">
+            <h1>${movie.title}</h1>
+            <p>${movie.description.substring(0,300)}...</p>
+        </div>
+        `
+    });
+    container.innerHTML=result
+}
+
+document.addEventListener('DOMContentLoaded',()=>{
+    getApi().then(data=>{
+        display(data)
+    })
+})
